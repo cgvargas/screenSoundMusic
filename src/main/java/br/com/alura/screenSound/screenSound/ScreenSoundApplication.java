7e0989da1d485c3f8bb00085aa9643fa97f6e86c
Claudio G. Vargas <CGVargas>
2024-06-22 17:43:04 -0300

    Criada as classes do pacote service

--- src/main/java/br/com/alura/screenSound/screenSound/ScreenSoundApplication.java
+++ src/main/java/br/com/alura/screenSound/screenSound/ScreenSoundApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ScreenSoundApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ScreenSoundApplication.class, args);
+		System.out.println("Bem vindos à minha aplicação java");
 	}
 
 }
--- src/main/java/br/com/alura/screenSound/screenSound/service/ConsumoApi.java
+++ src/main/java/br/com/alura/screenSound/screenSound/service/ConsumoApi.java
@@ -1,2 +1,28 @@
-package br.com.alura.screenSound.screenSound.service;public class ConsumoApi {
+package br.com.alura.screenSound.screenSound.service;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ConsumoApi {
+    public String obterDados(String endereco){
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(endereco))
+                .build();
+        HttpResponse<String> response = null;
+        try{
+            response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString()); // Corrigido aqui
+        } catch (IOException e){
+            throw new RuntimeException(e);
+        } catch (InterruptedException e){
+            throw new RuntimeException(e);
+        }
+
+        String json = response.body();
+        return json;
+    }
 }
